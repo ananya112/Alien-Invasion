c0d74d23e25b9fd15701a15bc3329a12b9296ab3
gajjaria <30932326+gajjaria@users.noreply.github.com>
2017-08-14 08:39:57 -0400

    Update database.php

--- cyber/application/config/database.php
+++ cyber/application/config/database.php
@@ -50,11 +50,11 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = 'sparrow123';
+$db['default']['password'] = '';
 $db['default']['database'] = 'test';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
-$db['default']['pconnect'] = TRUE;
+$db['default']['pconnect'] = FALSE;
 $db['default']['db_debug'] = TRUE;
 $db['default']['cache_on'] = FALSE;
 $db['default']['cachedir'] = '';
