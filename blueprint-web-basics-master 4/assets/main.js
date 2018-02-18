e22c280312bf723ba3c8c7c52c641b4651dbaedd
Tiger Peng <nytigeryxp@gmail.com>
2018-02-18 14:06:04 -0500

    added translation into unicode

--- blueprint-web-basics-master 4/assets/main.js
+++ blueprint-web-basics-master 4/assets/main.js
@@ -4,8 +4,12 @@ console.log("test")
 
 alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 characters = ["⌇", "⌆", "⌅", "⌁", "҂", "؏", "۽", "۾", "৺", "୰", "௳", "౿", "൹", "༓", "༜", "༴", "༸", "࿂", "᎓", "᎔", "᎕", "᎘", "᎙", "℥", "⅊", "⇃"];
-phrases = ["Take us to your leader", "Give us your cows"];
-
+simplephrases = ["Take us to your leader", "We come in peace", "We are an alien species"];
+mediumphrases = ["Do not be afraid", "Please we only wish to cooperate", "We wish to reinstate relations with Earth"];
+finalphrases = ["We are here to study the existence of your inferior race", "We come from a neighboring galaxy and intend to fulfill peaceful relations", "According to our analysis your species is not worthy of survival"];
+transsimple = [];
+transmedium = [];
+transfinal = [];
 dict = {};
 function assign() {
 	for(let x = 0; x < 26; x++) {
@@ -21,24 +25,26 @@ function assign() {
 
 function transto(phrase) {
 	transed = phrase.toUpperCase();
-	console.log(transed);
-	for(x = 0; x < phrase.length; x++) {
+	for(let x = 0; x < phrase.length; x++) {
 		if(transed[x] == " ") {
 			pass = "pass";
-			console.log(transed);
 		} else {
 			transed = transed.replace(transed[x], dict[transed[x]]);
-			console.log(transed);
 		}
 	}
+	return transed;
 }
 assign();
-console.log(phrases.length);
-for(x = 0; x < phrases.length - 1; x++) {
-	transto(phrases[x]);
+for(let i = 0; i < 3; i++) {
+	transsimple.push(transto(simplephrases[i]));
+	transmedium.push(transto(mediumphrases[i]));
+	transfinal.push(transto(finalphrases[i]));
+	console.log(transsimple);
+	console.log(transmedium);
+	console.log(transfinal);
 }
 var button = document.getElementById("start");
 button.addEventListener("click", function() {
 
 }, false);
-}
+}	
